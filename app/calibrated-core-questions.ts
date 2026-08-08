b8a302aedae8a3d06e0ddb79846e7839219b4e0d
masakasakasama <64453679+masakasakasama@users.noreply.github.com>
2026-08-08 16:46:44 +0900

    Aggregate calibrated core questions

---
+++ app/calibrated-core-questions.ts
@@ -0,0 +1,17 @@
+import { calibratedCoreUnit1Questions } from "./calibrated-core-unit-1.ts";
+import { calibratedCoreUnit2Questions } from "./calibrated-core-unit-2.ts";
+import { calibratedCoreUnit3Questions } from "./calibrated-core-unit-3.ts";
+import { calibratedCoreUnit4Questions } from "./calibrated-core-unit-4.ts";
+import { calibratedCoreUnit5Questions } from "./calibrated-core-unit-5.ts";
+import { calibratedCoreUnit6Questions } from "./calibrated-core-unit-6.ts";
+import { calibratedCoreUnit7Questions } from "./calibrated-core-unit-7.ts";
+
+export const calibratedCoreQuestions = [
+  ...calibratedCoreUnit1Questions,
+  ...calibratedCoreUnit2Questions,
+  ...calibratedCoreUnit3Questions,
+  ...calibratedCoreUnit4Questions,
+  ...calibratedCoreUnit5Questions,
+  ...calibratedCoreUnit6Questions,
+  ...calibratedCoreUnit7Questions,
+];
