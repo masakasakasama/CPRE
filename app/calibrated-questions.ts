f170496e3a034ae94e5973af5c8c3291ab745da2
masakasakasama <64453679+masakasakasama@users.noreply.github.com>
2026-08-08 16:42:31 +0900

    Aggregate calibrated CPRE questions

---
+++ app/calibrated-questions.ts
@@ -0,0 +1,17 @@
+import { calibratedUnit1Questions } from "./calibrated-unit-1.ts";
+import { calibratedUnit2Questions } from "./calibrated-unit-2.ts";
+import { calibratedUnit3Questions } from "./calibrated-unit-3.ts";
+import { calibratedUnit4Questions } from "./calibrated-unit-4.ts";
+import { calibratedUnit5Questions } from "./calibrated-unit-5.ts";
+import { calibratedUnit6Questions } from "./calibrated-unit-6.ts";
+import { calibratedUnit7Questions } from "./calibrated-unit-7.ts";
+
+export const calibratedQuestions = [
+  ...calibratedUnit1Questions,
+  ...calibratedUnit2Questions,
+  ...calibratedUnit3Questions,
+  ...calibratedUnit4Questions,
+  ...calibratedUnit5Questions,
+  ...calibratedUnit6Questions,
+  ...calibratedUnit7Questions,
+];
